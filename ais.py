f5b88571693b6ff1196093c4960197dff1ebcfa1
Sufian Zaabalawi <m.s.zaabalawi@52north.org>
2021-03-31 01:02:54 +0200

    optimize for large files WIP

--- ais.py
+++ ais.py
@@ -8,12 +8,10 @@ from pathlib import Path
 import geopandas as gpd
 import pandas as pd
 import requests
-import wget
 from bs4 import BeautifulSoup
 
 from check_connection import CheckConnection
 from config import config
-
 pd.options.mode.chained_assignment = None
 
 logger = logging.getLogger(__name__)
@@ -57,19 +55,25 @@ def download_AIS(year, work_dir):
         CheckConnection.is_online()
         logger.info('downloading AIS file: %s' % file)
         # download zip file using wget with url and file name
-        wget.download(url=os.path.join(url, file), bar=None)
-        file = file.split('/')[-1] if len(file.split('/')) > 1 else file
+        with requests.get(os.path.join(url, file), stream=True) as req:
+            req.raise_for_status()
+            file = file.split('/')[-1] if len(file.split('/')) > 1 else file
+            with open(file, "wb") as handle:
+                for chunk in req.iter_content(chunk_size=8192):
+                    handle.write(chunk)
+                handle.close()
         # extract each zip file into output directory then delete it
         with zipfile.ZipFile(file, 'r') as zip_ref:
             for f in zip_ref.infolist():
                 if f.filename.endswith('.csv'):
                     f.filename = os.path.basename(f.filename)
                     zip_ref.extract(f, p)
-                if f.filename.endswith('.gdb/'):
+                if str(Path(f.filename).parent).endswith('.gdb'):
                     zip_ref.extractall(p)
-                    gdb_file = Path(p, f.filename[:-1])
+                    name = str(Path(f.filename).parent)
+                    gdb_file = Path(p, name)
                     gdf = gpd.read_file(gdb_file)
-                    name = f.filename.split('.')[0]
+                    name = name.split('.')[0]
                     gdf['LON'] = gdf.geometry.apply(lambda point: point.x)
                     gdf['LAT'] = gdf.geometry.apply(lambda point: point.y)
                     gdf.drop(columns=['geometry'], inplace=True)
@@ -92,27 +96,29 @@ def subsample_AIS_to_CSV(year, work_dir, min_time_interval=30):
         year, min_time_interval))
     # check already processed files in the
     resume = check_dir(Path(work_dir, output))
-
+    chunkSize = 500000
     files = [f for f in sorted(os.listdir(str(
         Path(work_dir, year))), key=str.lower) if f.endswith('.csv') and f not in resume]
     for file in files:
         logging.info("Subsampling  %s " % str(file))
-        df = pd.read_csv(Path(Path(work_dir, year), file))
-        df = df.drop(['MMSI', 'VesselName', 'CallSign', 'Cargo', 'TranscieverClass',
-                     'ReceiverType', 'ReceiverID'], axis=1, errors='ignore')
-        df = df.dropna()
-        if 'VesselType' in df.columns:
-            df = df.query(
-                '(Status == "under way using engine" or Status == "under way sailing" or  Status == 8 or  Status == 0) & (VesselType == 1016 or 89 >= VesselType >= 70) & SOG > 3')
-        else:
-            df = df.query(
-                '(Status == "under way using engine" or Status == "under way sailing" or  Status == 8 or  Status == 0) & SOG > 3')
-        df = df.drop(['Status'], axis=1, errors='ignore')
-        # parse and set seconds to zero
-        df['BaseDateTime'] = pd.to_datetime(
-            df.BaseDateTime, format='%Y-%m-%dT%H:%M:%S').apply(rm_sec)
-        df.index = df.BaseDateTime
-        df = df.resample("%dT" % int(min_time_interval)).last()
-        df.reset_index(drop=True, inplace=True)
-        df = df.dropna()
-        df.to_csv(Path(path, str(file)))
+        header = True
+        for df_chunk in pd.read_csv(Path(Path(work_dir, year), file), chunksize=chunkSize):
+            df_chunk = df_chunk.drop(['MMSI', 'VesselName', 'CallSign', 'Cargo', 'TranscieverClass',
+                          'ReceiverType', 'ReceiverID'], axis=1, errors='ignore')
+            df_chunk = df_chunk.dropna()
+            if 'VesselType' in df_chunk.columns:
+                df_chunk = df_chunk.query(
+                    '(Status == "under way using engine" or Status == "under way sailing" or  Status == 8 or  Status == 0) & (VesselType == 1016 or 89 >= VesselType >= 70) & SOG > 3')
+            else:
+                df_chunk = df_chunk.query(
+                    '(Status == "under way using engine" or Status == "under way sailing" or  Status == 8 or  Status == 0) & SOG > 3')
+            df_chunk = df_chunk.drop(['Status'], axis=1, errors='ignore')
+            # parse and set seconds to zero
+            df_chunk['BaseDateTime'] = pd.to_datetime(
+                df_chunk.BaseDateTime, format='%Y-%m-%dT%H:%M:%S').apply(rm_sec)
+            df_chunk.index = df_chunk.BaseDateTime
+            df_chunk = df_chunk.resample("%dT" % int(min_time_interval)).last()
+            df_chunk.reset_index(drop=True, inplace=True)
+            df_chunk = df_chunk.dropna()
+            df_chunk.to_csv(Path(path, str(file)), chunksize=chunkSize, mode='a', header=header)
+            header = False
