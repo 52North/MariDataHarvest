5a7ef4fb6704fc75da49d762d9b02b6ddbdd9218
Sufian Zaabalawi <m.s.zaabalawi@52north.org>
2021-05-11 11:17:00 +0200

    periodiclly remove files

--- EnvDataServer/app.py
+++ EnvDataServer/app.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import threading
 from pathlib import Path
 
 sys.path.append(os.getcwd())
@@ -9,14 +10,14 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from utilities.helper_functions import str_to_date_min
 from EnvironmentalData.weather import get_global_wave, get_global_wind, get_GFS, get_global_phy_daily
-from datetime import timedelta
+from datetime import timedelta, datetime
 import xarray as xr
 import uuid
 import numpy as np
 import time
 from waitress import serve
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('EnvDataServer.app')
 
 app = Flask(__name__, static_folder=os.path.abspath("static/"))
 limiter = Limiter(
@@ -24,6 +25,28 @@ limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["70 per hour"]
 )
+
+# global variables TODO move to a config file
+delete_file_queue = dict()
+FILE_LIFE_SPAN = 20 * 60  # seconds
+spatial_interpolation_rate = 0.083
+temporal_interpolation_rate = 3  # hours
+
+
+def remove_files():
+    while True:
+        _deep_copy = delete_file_queue.copy()
+        for f_path, created_time in _deep_copy.items():
+            if (datetime.now() - created_time).seconds > FILE_LIFE_SPAN:
+                Path(f_path).unlink(missing_ok=True)
+                del delete_file_queue[f_path]
+                logger.debug('Deleting expired file %s ' % f_path)
+        time.sleep(10)
+
+
+dir_cleaner_thread = threading.Thread(target=remove_files)
+dir_cleaner_thread.daemon = True
+dir_cleaner_thread.start()
 
 
 def parse_requested_var(args):
@@ -51,10 +74,10 @@ def request_env_data():
     wave, wind, gfs, phy = parse_requested_var(request.args)
     date_lo = str_to_date_min(request.args.get('date_lo')) - timedelta(hours=1)
     date_hi = str_to_date_min(request.args.get('date_hi')) + timedelta(hours=1)
-    lat_lo = float(request.args.get('lat_lo')) - 0.0833
-    lat_hi = float(request.args.get('lat_hi')) + 0.0833
-    lon_lo = float(request.args.get('lon_lo')) - 0.0833
-    lon_hi = float(request.args.get('lon_hi')) + 0.0833
+    lat_lo = float(request.args.get('lat_lo')) - spatial_interpolation_rate
+    lat_hi = float(request.args.get('lat_hi')) + spatial_interpolation_rate
+    lon_lo = float(request.args.get('lon_lo')) - spatial_interpolation_rate
+    lon_hi = float(request.args.get('lon_hi')) + spatial_interpolation_rate
     if lat_lo > lat_hi:
         logger.debug('Error: lat_lo > lat_hi')
         return Response('Error: lat_lo > lat_hi')
@@ -69,10 +92,11 @@ def request_env_data():
         return Response('Error: No variables are selected')
     errorString = ''
 
-    lat_interpolation = list(np.arange(lat_lo, lat_hi, 0.083))
-    lon_interpolation = list(np.arange(lon_lo, lon_hi, 0.083))
+    lat_interpolation = list(np.arange(lat_lo, lat_hi, spatial_interpolation_rate))
+    lon_interpolation = list(np.arange(lon_lo, lon_hi, spatial_interpolation_rate))
     temporal_interpolation = [date_lo + timedelta(hours=hours) for hours in
-                              range(0, (date_hi - date_lo).days * 24 + (date_hi - date_lo).seconds // 3600, 3)]
+                              range(0, (date_hi - date_lo).days * 24 + (date_hi - date_lo).seconds // 3600,
+                                    temporal_interpolation_rate)]
 
     def rescale_dataset(dataset: xr.Dataset, gfs_flag=False) -> xr.Dataset:
         if gfs_flag:
@@ -87,9 +111,9 @@ def request_env_data():
             time=xr.DataArray(temporal_interpolation, coords=[temporal_interpolation], dims=["time"]))
 
     if int(lat_hi - lat_lo) > 20 or int(lon_hi - lon_lo) > 20 or (date_hi - date_lo).days > 10:
-        error = 'Error occurred: requested bbox ({0}° lat x {1}° lon x {2} days) is too large.'.format(
+        error = 'Error occurred: requested bbox ({0}° lat x {1}° lon x {2} days) is too large. Maximal bbox dimension ({3}° lat x {4}° lon x {5} days).'.format(
             int(lat_hi - lat_lo),
-            int(lon_hi - lon_lo), (date_hi - date_lo).days)
+            int(lon_hi - lon_lo), (date_hi - date_lo).days, 20, 20, 10)
         logger.debug(error)
         return Response(error)
 
@@ -145,18 +169,18 @@ def request_env_data():
     lat = 'Latitude extent: %.2f to %.2f' % (lat_lo, lat_hi) + header
     spatial_res = 'Spatial Resolution 0.083deg x 0.083deg' + header
     temporal_res = 'Temporal Resolution 3-hours interval' + header
-    credit_CMEMS = 'Credit (Wave, Wind and Physical): E.U. Copernicus Marine Service Information (CMEMS)' + header
+    credit_CMEMS = 'Credit (Wave-Wind-Physical): E.U. Copernicus Marine Service Information (CMEMS)' + header
     credit_GFS = 'Credit (GFS): National Centers for Environmental Prediction/National Weather Service/NOAA' + header
-    created = 'Accessed on %s' % time.strftime('%Y-%m-%d %H:%M:%S') + header
+    created = 'Accessed on %s' % datetime.now().strftime('%Y-%m-%d %H:%M:%S') + header
     err = ''
     if len(errorString) > 0:
         err = 'Error: ' + errorString + header
     csv_str = timeRange + lon + lat + spatial_res + temporal_res + credit_CMEMS + credit_GFS + created + err + csv_str
     with open(file_path, 'w', newline='', encoding='utf-8') as f:
         f.write(csv_str)
-    resp = errorString + 'Download requested CSV file: <a href="/EnvDataAPI/' + str(
-        file_path.name) + '"> /EnvDataAPI/' + str(
-        file_path.name) + '</a>'
+    resp = errorString + '<a href="/EnvDataAPI/' + str(
+        file_path.name) + '"> Download requested CSV file</a> <strong> The file will be deleted after 20 Minutes automatically.</strong>'
+    delete_file_queue[file_path] = datetime.now()
     logger.debug(resp)
     return resp
 
