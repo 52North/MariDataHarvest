a079cb21df4505c52ca0705f5399adbbbe65aae8
EHJ-52n <e.h.juerrens@52north.org>
2023-01-31 17:30:46 +0100

    Add documentation to URL_PREFIX

--- EnvDataServer/config.py
+++ EnvDataServer/config.py
@@ -29,6 +29,8 @@ import os
 BASE_URL = os.getenv("BASE_URL", "http://localhost:8080/")
 
 # https://docs.pylonsproject.org/projects/waitress/en/stable/arguments.html#arguments
+# empty URL_PREFIX => nothing is done
+# anything in URL_PREFIX => prefix is removed from context path before being passed to the code
 URL_PREFIX = os.getenv("URL_PREFIX", "")
 
 # 50 Mb limit
